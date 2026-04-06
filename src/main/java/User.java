642d9bfe677924627239a9b90497c2e25ecb21a6
drweb <dwebb@ithaca.edu>
2026-04-06 10:57:16 -0400

    added basic constructor, attributes, and getters/setters to 2 login classes

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,3 +1,25 @@
 public class User {
-    
+    private String username;
+    private String password;
+
+    public User(String user, String pass){
+        this.username = user;
+        this.password = pass;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setUsername(String user) {
+        this.username = user;
+    }
+
+    public void setPassword(String pass) {
+        this.password = pass;
+    }
 }
