4d3e7391a00f83499f3ce696e59823c813954b2b
drweb <dwebb@ithaca.edu>
2026-04-08 13:59:38 -0400

    implemented very basic initial UI in prep for login implementation

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,18 +1,19 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.*;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.PasswordField;
 import javafx.stage.Stage;
+import javafx.scene.control.TextField;
 
-public class MainApp extends Application {
-
+public class MainApp extends Application{
+    
     @Override
     public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setScene(scene);
+        stage.setScene(createLoginScene());
         stage.show();
     }
 
@@ -20,4 +21,52 @@ public class MainApp extends Application {
         launch();
     }
 
+    public Scene createMainScene(){
+        Pane newPane = new Pane();
+        newPane.setStyle("-fx-background-color: grey;");
+        Label l = new Label("Login Successful!");
+        TextField searchTF = new TextField();
+        Button bSearch = new Button("Search");
+
+
+        l.setLayoutX(200);
+        l.setLayoutY(0);
+        bSearch.setLayoutX(200);
+        bSearch.setLayoutY(200);
+        searchTF.setLayoutX(200);
+        searchTF.setLayoutY(175);
+
+        newPane.getChildren().add(bSearch);
+        newPane.getChildren().add(l);
+        newPane.getChildren().add(searchTF);
+        Scene MenuScene = new Scene(newPane, 600, 400);
+        return MenuScene;
+    }
+
+    public Scene createLoginScene(){
+        Pane p = new Pane(); 
+        p.setStyle("-fx-background-color: grey;");
+        Label l = new Label("Welcome to the Reader! Please enter username and password");
+        TextField usernameTF = new TextField();
+        PasswordField passwordTF = new PasswordField();
+        Button bLogin = new Button("Login");
+        bLogin.setOnAction(e -> ((Stage) bLogin.getScene().getWindow()).setScene(createMainScene()));
+
+    
+        l.setLayoutX(200);
+        l.setLayoutY(0);
+        bLogin.setLayoutX(200);
+        bLogin.setLayoutY(200);
+        usernameTF.setLayoutX(200);
+        usernameTF.setLayoutY(150);
+        passwordTF.setLayoutX(200);
+        passwordTF.setLayoutY(175);
+
+        p.getChildren().add(bLogin);
+        p.getChildren().add(l);
+        p.getChildren().add(usernameTF);
+        p.getChildren().add(passwordTF);
+        Scene LoginScene = new Scene(p, 600, 400);
+        return LoginScene;
+    }
 }
